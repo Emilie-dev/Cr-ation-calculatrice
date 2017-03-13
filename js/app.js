bf14205c94e4f3d48b2a4eff11a1412f5508e75b
Chauvin <chauvin-e@orange.fr>
2017-03-13 17:21:21 +0100

    Révision et modification du fichier javascript avec jquery

--- js/app.js
+++ js/app.js
@@ -1,21 +1,72 @@
-function addition (a, b) {
-	return a + b;
-}
+/*	
+	$("input").on("click", function(evenement) {
+		var valeur = $(this).val();
+		console.log(valeur);
+	$("#resultat").val(valeur);
+				
+	});
+*/
 
-function soustraction (a, b) {
-	return a - b;
-}
+var a;
+var b;
+var c;
+var result;
 
-function multiplication (a, b) {
-	return a * b;
-}
 
-function division(a, b) {
-	if (b > 0) {
-		return a / b;
-	}else{
-		console.log(division("Can't divide by 0"))
+	$(".nombre1 input").on("click", function(evenement) {
+		a = $(this).val();
+		console.log(valeur);
+					
+	});
+
+	$(".nombre2 input").on("click", function(evenement) {
+		b = $(this).val();
+		console.log(valeur);
+
+
+	$(".opération input").on("click", function(evenement) {
+		c = $(this).val();
+		console.log(valeur);
+
+
+function opé(){
+
+	if (c == "+") {
+		return a + b;
 	}
-}
 
-$("button")
+	$("#resultat").val(result){
+
+	});
+// function addition (a, b) {
+// 	return a + b;
+// }
+
+// function soustraction (a, b) {
+// 	return a - b;
+// }
+
+// function multiplication (a, b) {
+// 	return a * b;
+// }
+
+// function division(a, b) {
+// 	if (b > 0) {
+// 		return a / b;
+// 	}else{
+// 		console.log(division("Can't divide by 0"))
+// 	}
+// }
+
+// var calcul="";
+// var nombre=5;
+
+/*
+	$(".buttons").html("salut"); // j'ajoute le texte "salut" dans mes balises class button
+	var i = $(".buttons").html(); // je les récupère et la place dans var i;
+	console.log(i);
+*/
+ 	
+
+	
+
